261dbc01d54a795b2a7ef3ef52a6bbd7791b70a3
Victor Patru <patruvictor.mihai@gmail.com>
2022-09-21 11:59:50 +0300

    chore: project setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        veryDarkGray: "hsl(0, 0%, 17%)",
+        darkGray: "hsl(0, 0%, 59%)",
+      },
+      fontFamily: {
+        serif: ["Rubik", "sams-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
