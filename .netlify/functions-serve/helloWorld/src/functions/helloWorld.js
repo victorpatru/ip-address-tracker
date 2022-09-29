6fa330507bceed8e28e58834d618eddc0193afa9
Victor Patru <patruvictor.mihai@gmail.com>
2022-09-29 13:29:35 +0300

    chore: serverless boilerplate

---
+++ .netlify/functions-serve/helloWorld/src/functions/helloWorld.js
@@ -0,0 +1,8 @@
+// functions/helloWorld.cjs
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World!" })
+  };
+};
+//# sourceMappingURL=helloWorld.js.map
