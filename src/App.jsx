a0afc7775363f9637a73b136bd1634968f97bae1
Victor Patru <patruvictor.mihai@gmail.com>
2022-10-02 00:51:02 +0300

    fix: fetching ip address location

--- src/App.jsx
+++ src/App.jsx
@@ -15,36 +15,6 @@ import { publicIpv4 } from "public-ip";
 import Spinner from "./components/Spinner";
 
 function App() {
-  const [userData, setUserData] = useState({
-    ipAddress: "8.8.8.8",
-    location: {
-      country: "US",
-      region: "California",
-      city: "Mountain View",
-      lat: 37.40599,
-      lng: -122.078514,
-      postalCode: "94043",
-      timezone: "-07:00",
-    },
-    isp: "Google LLC",
-  });
-
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // Get the current user's IP Address
-    const getCurrentUserIp = async () => {
-      try {
-        const info = await publicIpv4();
-        console.log(info);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    //getCurrentUserIp();
-  }, []);
-
   // Helper function ensuring the inputted value is a valid IP Address
   function ValidateIPaddress(ipaddress) {
     if (
@@ -58,28 +28,90 @@ function App() {
     return false;
   }
 
+  const [userData, setUserData] = useState({
+    ipAddress: "8.8.8.8",
+    location: {
+      country: "US",
+      region: "California",
+      city: "Mountain View",
+      lat: 37.40599,
+      lng: -122.078514,
+      timezone: "-07:00",
+    },
+    isp: "Google LLC",
+  });
+
+  const [queryIpAddress, setQueryIpAddress] = useState("");
+  let envApiKey = import.meta.env.VITE_API_KEY;
+
+  // Use effect that only runs on first load (getting the ip address)
+  const fetchUserIp = async () => {
+    try {
+      const data = await publicIpv4();
+      setQueryIpAddress(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Use effect that runs every time we change the ip adress
+  useEffect(() => {
+    const getIpInfo = async () => {
+      try {
+        const res = await fetch(
+          `https://api.apilayer.com/ip_to_location/${queryIpAddress}`,
+          {
+            headers: {
+              apikey: envApiKey,
+            },
+          }
+        );
+        const data = await res.json();
+        setUserData((prevState) => ({
+          ...prevState,
+          ipAddress: data.ip,
+          location: {
+            country: data.country_code,
+            region: data.region_name,
+            city: data.city,
+            lat: data.latitude,
+            lng: data.longitude,
+            timezone: data.timezones[0],
+          },
+          isp: data.connection.isp,
+        }));
+      } catch (error) {
+        console.trace(error);
+      }
+    };
+    if (queryIpAddress !== "") {
+      getIpInfo();
+    }
+  }, [queryIpAddress]);
+
   // Populate our userData object with the inputted ip address
   const handleSearchChange = (e) => {
     e.preventDefault();
 
     ValidateIPaddress(e.target.firstChild.value)
-      ? setUserData({
-          ...userData,
-          ipAddress: e.target.firstChild.value,
-        })
+      ? setQueryIpAddress(e.target.firstChild.value)
       : alert("You have entered an invalid IP address!");
   };
 
-  if (loading) {
-    return <Spinner />;
-  }
+  const getUserIp = async (e) => {
+    e.preventDefault();
+    fetchUserIp();
+  };
 
   return (
     <>
       <section className="min-h-screen flex flex-col justify-start items-center font-serif bg-red-300">
         {/* Search Component */}
         <section className="relative w-full flex flex-col justify-center items-center">
-          <Search handleSearchChange={handleSearchChange} />
+          <Search
+            handleSearchChange={handleSearchChange}
+            getUserIp={getUserIp}
+          />
           <DisplayInfo userData={userData} />
           <DisplayLocation userData={userData} />
         </section>
